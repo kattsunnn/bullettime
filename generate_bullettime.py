c3820a0efde46012d1c8e933a9f621bf1037ae06
naoki <katsudanaoki@gmail.com>
2025-12-13 19:55:31 +0900

    fix: e2pをサブモジュール化

--- generate_bullettime.py
+++ generate_bullettime.py
@@ -3,24 +3,26 @@ import numpy as np
 import cv2
 from sklearn.cluster import DBSCAN
 
-from e2p import E2P
 from pd import PD
 from ppi import PPI
 import search_similar_img as ssi
 import img_utils.img_utils as iu
 import super_resolution.super_resolution as sr
 from person_re_identification.osnet_reid import OSNetReID
+from omni_directional_img_utils.e2p import E2P
 
 FOV = 30
 _map_cache = {}
 
-def get_or_create_map(src_size_w, src_size_h, dst_width, dst_height, theta_eye, phi_eye, fov):
-    cache_key = (src_size_w, src_size_h, dst_width, dst_height, theta_eye, phi_eye, fov)
+def get_or_create_map(src_img_w, src_img_h, fov, theta_eye, phi_eye):
+    cache_key = (src_img_w, src_img_h, fov, theta_eye, phi_eye)
     
     if cache_key not in _map_cache:
-        view = E2P(src_size_w, src_size_h, dst_width, dst_height)
-        view.generate_map(np.deg2rad(theta_eye), np.deg2rad(phi_eye), 0, 1)
-        _map_cache[cache_key] = view
+        map = E2P(src_img_w, src_img_h)
+        dst_w = map.calc_optimal_width(fov)
+        dst_h = map.calc_optimal_height(fov)
+        map.generate_map(dst_w, dst_h, theta_eye, phi_eye, 0)
+        _map_cache[cache_key] = map
     
     return _map_cache[cache_key]
 
@@ -28,29 +30,23 @@ def get_or_create_map(src_size_w, src_size_h, dst_width, dst_height, theta_eye, 
 # 透視投影画像群の生成
 def generate_front_ppis(img_e, fov=FOV, overlap=0.5):
     # 使用する全方位カメラの解像度
-    src_size_h = img_e.shape[0]
-    src_size_w = img_e.shape[1]
-    # 表示する透視投影画像の解像度
-    dst_width = int(2.0 * np.tan(np.deg2rad(fov) / 2.0) * src_size_w / (2*np.pi))
-    dst_height = int(2.0 * np.tan(np.deg2rad(fov) / 2.0) * src_size_h / np.pi)
-    # view = E2P(src_size_w, src_size_h, dst_width, dst_height)
-
+    src_img_h = img_e.shape[0]
+    src_img_w = img_e.shape[1]
+    # 透視投影画像を生成する視線角度の設定
     THETA_RANGE = 90
     PHI_RANGE = 60
     angle_step = fov * (1 - overlap)
     first_step = fov/2
     theta_eyes = np.arange(-THETA_RANGE + first_step, THETA_RANGE, angle_step)
     phi_eyes = np.arange(PHI_RANGE - first_step, -PHI_RANGE, -angle_step)
-    # 透視投影画像を生成する視線角度の設定
 
     ppis = []
 
     for j, phi_eye in enumerate(phi_eyes):
         for i, theta_eye in enumerate(theta_eyes):
             # 透視投影画像の生成
-            view = get_or_create_map(src_size_w, src_size_h, dst_width, dst_height, 
-                                     theta_eye, phi_eye, fov)
-            dst_img = view.generate_image(img_e)
+            map = get_or_create_map(src_img_w, src_img_h, fov, theta_eye, phi_eye)
+            dst_img = map.generate_img(img_e)
             # 保存
             ppi = PPI(img_e, dst_img, theta_eye, phi_eye)
             ppis.append(ppi)
@@ -58,15 +54,13 @@ def generate_front_ppis(img_e, fov=FOV, overlap=0.5):
 
 # 透視投影画像を生成
 def generate_ppi(img_e, theta_eye, phi_eye, scale=1, fov=FOV):
-    img_e_h = img_e.shape[0]
     img_e_w = img_e.shape[1]
-    # 透視投影画像の解像度
-    dst_width = int(2.0 * np.tan(np.deg2rad(fov) / 2.0) * img_e_w / (2*np.pi))
-    dst_height = int(2.0 * np.tan(np.deg2rad(fov) / 2.0) * img_e_h / np.pi)
-    # 透視投影画像の生成．
-    view = E2P(img_e_w, img_e_h, dst_width, dst_height)
-    view.generate_map(np.deg2rad(theta_eye), np.deg2rad(phi_eye), 0, scale)
-    ppi = view.generate_image(img_e)
+    img_e_h = img_e.shape[0]
+    map = E2P(img_e_w, img_e_h)
+    dst_w = map.calc_optimal_width(fov)
+    dst_h = map.calc_optimal_height(fov)
+    map.generate_map(dst_w, dst_h, theta_eye, phi_eye, 0, scale)
+    ppi = map.generate_img(img_e)
     return PPI(img_e, ppi, theta_eye, phi_eye)
 
 # スケールから適切なFOVを計算。
@@ -232,47 +226,6 @@ def generate_same_person_imgs(imgs, output_path):
         for path in paths:
             print(f"  {path}")
 
-#Todo:カメラ番号を渡して、画像を出力する
-# def generate_same_person_imgs(imgs, output_path):
-#     scaled_gaze_imgs_list = [generate_scaled_gaze_imgs(img, output_path, f"camera_{idx}") for idx, img in enumerate(imgs)]
-#     # デバック
-#     for idx, scaled_gaze_imgs in enumerate(scaled_gaze_imgs_list):
-#         cropped_gaze_imgs = filter_none(map(generate_crop_img, scaled_gaze_imgs))
-#         iu.save_imgs(cropped_gaze_imgs, f"{output_path}/04_cropped", f"camera_{idx}_{{}}")
-
-#     #スケール画像とクロップ画像のペアを作成。processed_gaze_imgs_list[カメラ番号][画像番号]["scaled"or"cropped"]
-#     processed_gaze_imgs_list = []
-#     for cam_idx, imgs in enumerate(scaled_gaze_imgs_list):
-#         processed_gaze_imgs = []
-#         for img in imgs:
-#             cropped_img = generate_crop_img(img)
-#             if cropped_img is not None: 
-#                 processed_gaze_imgs.append({ "scaled": img, "cropped": cropped_img  })
-#         if processed_gaze_imgs:
-#             processed_gaze_imgs_list.append(processed_gaze_imgs)
-
-#     # model_pahth = "super_resolution/models/EDSR_x4.pb"
-
-#     if processed_gaze_imgs_list[0]:
-#         query_imgs_cropped = [img["cropped"] for img in processed_gaze_imgs_list[0] ]
-#         # query_imgs_cropped = [ sr.edsr_x4(img["cropped"], model_pahth) for img in processed_gaze_imgs_list[0] ]
-#         cand_imgs_list = processed_gaze_imgs_list
-
-#     for idx, query_img_cropped in enumerate(query_imgs_cropped):
-#         same_person_imgs = []
-#         matching_imgs = []
-#         for cand_imgs in cand_imgs_list:
-#             cand_imgs_cropped = [img["cropped"] for img in cand_imgs ]
-#             # cand_imgs_cropped = [ sr.edsr_x4(img["cropped"], model_pahth) for img in cand_imgs]
-#             result = ssi.search_by_sift_ratiotest(query_img_cropped, cand_imgs_cropped)
-#             if result is None: continue
-#             most_similar_img_idx, most_similar_img, matching_img = result
-#             same_person_imgs.append(cand_imgs[most_similar_img_idx]["scaled"])
-#             matching_imgs.append(matching_img)
-#             query_img_cropped = most_similar_img
-#         print(len(same_person_imgs))
-#         iu.save_imgs(same_person_imgs, f"{output_path}/05_same_person", f"person_{idx}_{{}}")
-#         iu.save_imgs(matching_imgs, f"{output_path}/06_matching", f"person_{idx}_matching_{{}}")
 
 # Todo: 全体を分割する
 if __name__ == '__main__':
